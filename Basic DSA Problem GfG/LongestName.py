8064d750c85374186b0781d9ed608bd3b29b102c
Utkarsh Mishra <utkarshmishra7503@gmail.com>
2025-10-03 18:51:55 +0530

    Modified a little bit

--- Basic DSA Problem GfG/LongestName.py
+++ Basic DSA Problem GfG/LongestName.py
@@ -1,17 +1,23 @@
-# Using loop
 A = ['I','am','Utkarsh','Hello','World','America','Utkarsh']
-longest = ""
+
+# Using loop
+'''longest = ""
 for name in A:
     if len(name) > len(longest):
        longest = name
-print(longest)
+print(longest)'''
+
 # Using Dictionary
+# A Dictionary cannot store multiple item in a single key. 
+# To store mutliple items to akey, use list
 length_dict = {}
+
 for name in A:
-    if len(name) in length_dict:
-         length_dict[len(name)] += " , " + name
+    if len(name) not in length_dict:
+          length_dict[len(name)] = [name]
+         
     else:
-          length_dict[len(name)] = name
+          length_dict[len(name)].append(name)
 print(length_dict)
 print(max(length_dict))
     
